e28e4727bf6bba477070831cbf63093e9312b317
bhargobdeka <69599126+bhargob-deka@users.noreply.github.com>
2023-12-02 16:48:51 -0500

    CI/CD deployment added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTraini
 from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from text_summarizer.pipeline.stage_04_model_training import ModelTrainingPipeline
+from text_summarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from text_summarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -35,14 +36,24 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-STAGE_NAME = "Model Trainer stage"
+# STAGE_NAME = "Model Trainer stage"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_trainer = ModelTrainingPipeline()
+#    model_trainer.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+STAGE_NAME = "Model Evaluation stage"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   model_trainer = ModelTrainingPipeline()
-   model_trainer.main()
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
-
---
+++ src/text_summarizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,16 @@
+from text_summarizer.config.configuration import ConfigurationManager
+from text_summarizer.components.model_evaluation import ModelEvaluation
+from text_summarizer.logging import logger
+
+
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
